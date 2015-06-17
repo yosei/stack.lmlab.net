d414f856ed665e1881f5533aa816607ebc01222f
Yosei Ito <yousei.itou@gmail.com>
2015-06-18 08:34:31 +0900

    Add some seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,38 @@
 
 if Rails.env == "development"
   u = User.create(email: "test@lmlab.net", password: "secret123")
+p1 = <<EOT
+マークダウンが使えると嬉しいかも？
 
-  s1 = Stack.create(title:"xxをyyする方法", problem:"", solution:"", explanation:"",user:u)
+コードスニペット
+```
+alias rs="rails s -b 192.168.33.10"
+```
+EOT
+
+s1 = <<EOT
+テーブル。
+<table>
+  <tr>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+  </tr>
+  <tr>
+    <td>100,000</td>
+    <td>200,000</td>
+    <td>300,000</td>
+  </tr>
+</table>
+EOT
+
+e1 = <<EOT
+- 箇条書き1
+- 箇条書き2
+- 箇条書き3
+EOT
+
+  s1 = Stack.create(title:"レギュレーション", problem:p1, solution:s1, explanation:e1,user:u)
   StacksTag.create(stack:s1,name: "ruby")
   StacksTag.create(stack:s1,name: "rails")
 
