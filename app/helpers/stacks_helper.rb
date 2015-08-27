0c0382530f7058b425174d1635b0ec87e2af583d
Yosei Ito <yousei.itou@gmail.com>
2015-08-27 23:57:00 +0900

    Add redcarpet gem for using markdown.

--- app/helpers/stacks_helper.rb
+++ app/helpers/stacks_helper.rb
@@ -1,9 +1,16 @@
 module StacksHelper
+  @@markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+
   def tags(stack)
-    html = ""
+    ts = []
     stack.stacks_tags.each do |t|
-      html += link_to t.name, "#", class: "label label-default"
+      ts += [link_to(t.name, "#", class: "label label-default")]
     end
+    html = ts.join(" ")
     return html.html_safe
+  end
+
+  def markdown(text)
+    @@markdown.render(text).html_safe
   end
 end
