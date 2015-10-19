9ced147d92d209c5accaffee4f31b156026ec4d6
Shoya Kai <shonari77@gmail.com>
2015-10-19 06:22:14 +0000

    画像のアップロードのシードの変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,10 @@ p1 = <<EOT
 ```
 alias rs="rails s -b 192.168.33.10"
 ```
+画像表示例
+![](/stacks/1/1)
+![](/stacks/1/2)
+![](/stacks/1/3)
 EOT
 
 s1 = <<EOT
@@ -40,14 +44,15 @@ e1 = <<EOT
 - 箇条書き3
 EOT
 
-  p1 = "#{Rails.root}/db/seeds_images/1.png"
-  p2 = "#{Rails.root}/db/seeds_images/2.png"
-  p3 = "#{Rails.root}/db/seeds_images/3.png"
+  pic1 = "#{Rails.root}/db/seeds_images/1.png"
+  pic2 = "#{Rails.root}/db/seeds_images/2.png"
+  pic3 = "#{Rails.root}/db/seeds_images/3.png"
 
   s1 = Stack.create(title:"レギュレーション", problem:p1, solution:s1, explanation:e1,user:u)
-  s1.picture1 = File.read(p1)
-  s1.picture2 = File.read(p2)
-  s1.picture3 = File.read(p3)
+  s1.picture1 = File.read(pic1)
+  s1.picture2 = File.read(pic2)
+  s1.picture3 = File.read(pic3)
+  s1.save
   StacksTag.create(stack:s1,name: "ruby")
   StacksTag.create(stack:s1,name: "rails")
 
