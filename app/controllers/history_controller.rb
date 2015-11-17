eeecc9c78317f045773c82623eb750dbe29df015
Kai <s-kai@lumber-mill.co.jp>
2015-11-17 10:22:45 +0900

    履歴一覧から差分表示に移行したときの処理を追加

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -13,11 +13,16 @@ class HistoryController < ApplicationController
 
   def diff
     @origin = Stack.find(params[:id])
-    after_data = @origin
-    before_data = @origin.versions.last.reify
-    if params[:id] == 0
-    # unless after_data == nil || before_data == nil
+    num = @origin.versions.length - 1
+    if params[:num].to_i == 0
+      before_data = @origin.versions.last.reify
+      after_data = @origin
     else
+      after_data = @origin.versions[0 - params[:num].to_i].reify
+      before_data = after_data.previous_version
+    end
+
+    unless params[:num].to_i == num
       #title
       text = Diffy::Diff.new(before_data.title, after_data.title).to_s(:html_simple)
       @title = diff_text(text, after_data.title, "text")
@@ -41,6 +46,31 @@ class HistoryController < ApplicationController
       @picture1 = diff_pic(before_data.picture1, after_data.picture1)
       @picture2 = diff_pic(before_data.picture2, after_data.picture2)
       @picture3 = diff_pic(before_data.picture3, after_data.picture3)
+
+    else
+      #title
+      text = Diffy::Diff.new("", after_data.title).to_s(:html_simple)
+      @title = diff_text(text, after_data.title, "text")
+      #problem
+      text = Diffy::Diff.new("", after_data.problem).to_s(:html_simple)
+      @problem = diff_text(text, after_data.problem, "text")
+      #solution
+      text = Diffy::Diff.new("", after_data.solution).to_s(:html_simple)
+      @solution = diff_text(text, after_data.solution, "text")
+      #explanation
+      text = Diffy::Diff.new("", after_data.explanation).to_s(:html_simple)
+      @explanation = diff_text(text, after_data.explanation, "text")
+      #url
+      text = Diffy::Diff.new("", after_data.url1).to_s(:html_simple)
+      @url1 = diff_text(text, after_data.url1, "url").html_safe
+      text = Diffy::Diff.new("", after_data.url2).to_s(:html_simple)
+      @url2 = diff_text(text, after_data.url2, "url").html_safe
+      text = Diffy::Diff.new("", after_data.url3).to_s(:html_simple)
+      @url3 = diff_text(text, after_data.url3, "url").html_safe
+      #picture
+      @picture1 = diff_pic("", after_data.picture1)
+      @picture2 = diff_pic("", after_data.picture2)
+      @picture3 = diff_pic("", after_data.picture3)
     end
   end
 
