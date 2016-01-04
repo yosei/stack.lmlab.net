2561923926f0425b719bdba295bfab9f54699ac3
Yosei Ito <yousei.itou@gmail.com>
2016-01-04 09:34:23 +0900

    スタイルの微調整と、aboutページの追加（文章はまだ）。

--- app/controllers/stacks_controller.rb
+++ app/controllers/stacks_controller.rb
@@ -144,6 +144,9 @@ class StacksController < ApplicationController
     end
   end
 
+  def about
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_stack
